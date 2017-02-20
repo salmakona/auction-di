dda51debab9d0dbb66e5d42a06a8a0e719fa961f
Salma Kona Gugly <gugly2009@gmail.com>
2017-02-20 15:30:19 +0600

    Update app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -13,7 +13,7 @@ import StarsComponent from "./components/stars/stars";
 import {ProductService} from "./services/product-service";
 import HomeComponent from "./components/home/home";
 import ProductDetailComponent from "./components/product-detail/product-detail";
-
+import {FilterPipe} from "./components/pipes/filter-pipe";
 @NgModule({
     imports:      [ BrowserModule, ReactiveFormsModule,
                     RouterModule.forRoot([
@@ -28,7 +28,7 @@ import ProductDetailComponent from "./components/product-detail/product-detail";
                     ProductDetailComponent,
                     ProductItemComponent,
                     SearchComponent,
-                    StarsComponent],
+                    StarsComponent,FilterPipe],
     providers:    [ProductService,
                    {provide: LocationStrategy, useClass: HashLocationStrategy}],
     bootstrap:    [ ApplicationComponent ]
